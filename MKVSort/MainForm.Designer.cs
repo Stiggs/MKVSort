2d5e8f8810c043908afde47d34928ddd46f39a09
Elliot <elliotspaull@gmail.com>
2013-09-25 14:50:49 +0100

    Changed a font!

--- MKVSort/MainForm.Designer.cs
+++ MKVSort/MainForm.Designer.cs
@@ -52,10 +52,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Britannic Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(70, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(141, 39);
+            this.label1.Size = new System.Drawing.Size(146, 36);
             this.label1.TabIndex = 0;
             this.label1.Text = "MKV Sort";
             // 
